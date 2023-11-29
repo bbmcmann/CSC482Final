a1a83d2d489a32994c56236c4f371fe69c5ea7ea
Chris Kraft <kibby@The-KibPuter.local>
2023-11-29 13:45:35 -0800

    home region generator

--- data/generate_basic.py
+++ data/generate_basic.py
@@ -1,10 +1,27 @@
 import numpy as np
+import pandas as pd
+import random
 rng = np.random.default_rng()
+basestats = pd.read_csv('basestats.csv')
+av_gpa = basestats['GPA'].iloc[0]
 
-def generate_gpa(av_gpa):
+
+def generate_gpa():
     gpa = rng.normal(av_gpa, 0.2)
     return "{:.02f}".format(gpa)
 
 
+def get_home_region():
+    regions = basestats.columns[1:]
+    probs = []
+    for region in regions:
+        pct = basestats[region].iloc[0]
+        probs.append(float(pct.strip('%'))/100)
+
+    return random.choices(regions, weights=probs, k=1)[0]
+
+
+
 if __name__ == "__main__":
-    print(generate_gpa(3.13))
+    print(generate_gpa())
+    print(get_home_region())
