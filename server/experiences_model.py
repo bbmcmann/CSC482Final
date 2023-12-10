cee72c5eeff36693c945e4cd7d847c375912f32c
Ben McMann <bbmcmann@volosys.com>
2023-12-10 02:34:38 -0800

    setup modeling pipeline

--- server/experiences_model.py
+++ server/experiences_model.py
@@ -12,7 +12,6 @@ import time, math
 
 
 # GRU model for text generation
-
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, n_layers=1):
         super(RNN, self).__init__()
@@ -25,15 +24,42 @@ class RNN(nn.Module):
         self.gru = nn.GRU(hidden_size * 2, hidden_size, n_layers, batch_first=True,
                           bidirectional=False)
         self.decoder = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
 
     def forward(self, input, hidden):
         input = self.encoder(input.view(1, -1))
         output, hidden = self.gru(input.view(1, 1, -1), hidden)
         output = self.decoder(output.view(1, -1))
+        output = self.relu(output)
         return output, hidden
 
     def init_hidden(self):
         return Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+
+# class RNN(nn.Module):
+#     def __init__(self, input_size, hidden_size, output_size, n_layers):
+#         super(RNN, self).__init__()
+#         self.hidden_size = hidden_size
+#
+#         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+#         self.i2o = nn.Linear(input_size + hidden_size, output_size)
+#         self.o2o = nn.Linear(hidden_size + output_size, output_size)
+#         self.dropout = nn.Dropout(0.1)
+#         self.softmax = nn.LogSoftmax(dim=1)
+#         self.n_layers= n_layers
+#
+#     def forward(self, input, hidden):
+#         input_combined = torch.cat((input, hidden), 1)
+#         hidden = self.i2h(input_combined)
+#         output = self.i2o(input_combined)
+#         output_combined = torch.cat((hidden, output), 1)
+#         output = self.o2o(output_combined)
+#         output = self.dropout(output)
+#         output = self.softmax(output)
+#         return output, hidden
+#
+#     def init_hidden(self):
+#         return torch.zeros(1, self.hidden_size)
 
 
 # preps data for experience bullet points
@@ -67,8 +93,7 @@ def train_once(inp, target, decoder, criterion, decoder_optimizer, chunk_len):
 
 
 # trains entire model over all epochs
-def train_all(input_data, output_data, voc_len, chunk_len,
-              n_epochs=300,
+def train_all(input_data, output_data, voc_len, chunk_len, n_epochs=300,
               hidden_size=100,
               n_layers=1,
               lr=0.015):
@@ -90,7 +115,7 @@ def make_word_dict(cleaned_string):
 
 
 # build entire model
-def build_experience_model(data_path):
+def build_experience_model(data_path, n_epochs):
     cleaned_string = prep_data().split()
     trigrams = [([cleaned_string[i], cleaned_string[i + 1]], cleaned_string[i + 2])
                 for i in range(len(cleaned_string) - 2)]
@@ -110,7 +135,7 @@ def build_experience_model(data_path):
         targ = torch.tensor([word_to_ix[target]], dtype=torch.long)
         tar.append(targ)
 
-    trained_model = train_all(inp, tar, voc_len, chunk_len)
+    trained_model = train_all(inp, tar, voc_len, chunk_len, n_epochs=n_epochs)
 
     torch.save(trained_model.state_dict(), data_path)
 
@@ -137,18 +162,28 @@ def generate_sentence(decoder, word_to_ix, prime_str='Developed a', predict_len=
 
 
 # test the model
-def test_model():
+def test_model(model_path):
     clean_string = prep_data().split()
     vocab = set(clean_string)
     voc_len = len(vocab)
     the_model = RNN(voc_len, 100, voc_len, 1)
-    the_model.load_state_dict(torch.load(".\\pytorch_models\experience_model.pth"))
+    the_model.load_state_dict(torch.load(model_path))
     word_ix = make_word_dict(clean_string)
+
     print(generate_sentence(the_model, word_ix, "Developed a"))
+    print(generate_sentence(the_model, word_ix, "Implemented a"))
+    print(generate_sentence(the_model, word_ix, "Worked on"))
+    print(generate_sentence(the_model, word_ix, "Assisted in"))
+    print(generate_sentence(the_model, word_ix, "Developed a"))
+    print(generate_sentence(the_model, word_ix, "Implemented a"))
+    print(generate_sentence(the_model, word_ix, "Worked on"))
+    print(generate_sentence(the_model, word_ix, "Assisted in"))
+
 
 # how to run:
 # uncomment first line to build the model, can tweak params,
 # uncomment second to test model, can switch the sentence generation in test function
 if __name__ == '__main__':
-    #build_experience_model(".\\pytorch_models\experience_model.pth")
-    test_model()
+    working_path = ".\\pytorch_models\experience_model__RELU2.pth"
+    build_experience_model(working_path, 100)
+    # test_model(working_path)
---
+++ server/model_maker.py
@@ -0,0 +1,36 @@
+import experiences_model
+
+# IMPORTANT NOTE: if the bullet points training changes the models may not work anymore and might have to be retrained
+
+
+# FORMAT: Each item is the settings for a new model
+# ( PATH, NUM_EPOCHS )
+
+batch_to_train_param_list = [
+                    (".\\pytorch_models\experience_model_Maker200.pth", 200)]
+
+already_trained_param_list = [(".\\pytorch_models\experience_model_Maker300.pth", 300),
+                    (".\\pytorch_models\experience_model_Maker100.pth", 100),
+                    (".\\pytorch_models\experience_model_Maker50.pth", 50)]
+
+
+# test all models in train_param_list
+def test_models():
+    print("____TESTING ALL____")
+    for settings in already_trained_param_list:
+        print("\n\t\tTESTING: " + settings[0][17:] + "\n")
+        experiences_model.test_model(settings[0])
+
+
+# train all models in train_param_list
+def train_models():
+    print("____TRAINING ALL____")
+    for settings in batch_to_train_param_list:
+        print("TRAINING SETTINGS: " + str(settings))
+        experiences_model.build_experience_model(settings[0], settings[1])
+
+
+if __name__ == '__main__':
+    train_models()
+
+    # test_models()
